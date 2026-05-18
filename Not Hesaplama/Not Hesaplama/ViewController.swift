a48a19a1104cfaedf056a6d186391a0b5a14aec8
Arda Dağdeviren <rdty222@gmail.com>
2026-05-18 23:03:18 +0300

    Update folder structure

---
+++ Not Hesaplama/Not Hesaplama/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Not Hesaplama
+//
+//  Created by Arda Dağdeviren on 18.05.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
